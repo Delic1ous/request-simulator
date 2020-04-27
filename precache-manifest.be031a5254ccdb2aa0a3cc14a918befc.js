bf964cee4c2934af3d8cd9c3d4b3e12d9e892538
Delic1ous <s.delic1ous.d@gmail.com>
2020-04-27 09:26:23 +0300

    Updates

---
+++ precache-manifest.be031a5254ccdb2aa0a3cc14a918befc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba9ef32b3aabf1d7871e8fd82d66e20a",
+    "url": "/request-simulator/index.html"
+  },
+  {
+    "revision": "d112c3a7772062d5ff49",
+    "url": "/request-simulator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "be526418c36d3d9a4e7f",
+    "url": "/request-simulator/static/js/2.174e4a69.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/request-simulator/static/js/2.174e4a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d112c3a7772062d5ff49",
+    "url": "/request-simulator/static/js/main.af123320.chunk.js"
+  },
+  {
+    "revision": "3b43653b89214d9f316b",
+    "url": "/request-simulator/static/js/runtime-main.6c581923.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/request-simulator/static/media/logo.5d5d9eef.svg"
+  }
+]);
